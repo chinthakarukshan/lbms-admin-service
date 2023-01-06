8b4e2e69a925d5fd004219934c9ce65289671239
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2023-01-07 00:42:54 +0530

    Initial project structure

---
+++ src/main/java/com/lbms/library/lbmsadminservice/LbmsAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lbms.library.lbmsadminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbmsAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbmsAdminServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lbms/library/lbmsadminservice/LbmsAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbms.library.lbmsadminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbmsAdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
