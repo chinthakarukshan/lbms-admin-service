6c373643ed7a80f637977e679a14b2de870212a8
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2023-01-23 23:59:06 +0530

    Updated the schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `lbms_admin_db`.`member` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `userid` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `firstname` VARCHAR(45) NULL,
+  `lastname` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
