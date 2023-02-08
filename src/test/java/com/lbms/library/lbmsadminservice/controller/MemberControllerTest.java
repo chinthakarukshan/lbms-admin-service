573969d00affeb4f67f170141afc4ce7b6e91982
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2023-02-09 00:31:23 +0530

    Add Member Unit Test cases

--- src/test/java/com/lbms/library/lbmsadminservice/controller/MemberControllerTest.java
+++ src/test/java/com/lbms/library/lbmsadminservice/controller/MemberControllerTest.java
@@ -12,7 +12,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Date;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MemberController.class)
@@ -37,5 +39,69 @@ public class MemberControllerTest {
         this.mockMVC.perform(post("/members").contentType(MediaType.APPLICATION_JSON)
                                              .content(json))
                     .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void addMemberTest_validationFailure_Email() throws Exception {
+        MemberRequest memberRequest = new MemberRequest();
+        memberRequest.setFirstName("test first");
+        memberRequest.setLastName("test Last");
+        memberRequest.setDateOfBirth(new Date());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(memberRequest);
+
+        this.mockMVC.perform(post("/members").contentType(MediaType.APPLICATION_JSON)
+                                             .content(json))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().string(containsString("Email is Required")));
+    }
+
+    @Test
+    public void addMemberTest_validationFailure_FirstName() throws Exception {
+        MemberRequest memberRequest = new MemberRequest();
+        memberRequest.setEmail("testemail@test.com");
+        memberRequest.setLastName("test Last");
+        memberRequest.setDateOfBirth(new Date());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(memberRequest);
+
+        this.mockMVC.perform(post("/members").contentType(MediaType.APPLICATION_JSON)
+                                             .content(json))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().string(containsString("First Name is Required")));
+    }
+
+    @Test
+    public void addMemberTest_validationFailure_LastName() throws Exception {
+        MemberRequest memberRequest = new MemberRequest();
+        memberRequest.setEmail("testemail@test.com");
+        memberRequest.setFirstName("test first");
+        memberRequest.setDateOfBirth(new Date());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(memberRequest);
+
+        this.mockMVC.perform(post("/members").contentType(MediaType.APPLICATION_JSON)
+                                             .content(json))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().string(containsString("Last Name is Required")));
+    }
+
+    @Test
+    public void addMemberTest_validationFailure_DOB() throws Exception {
+        MemberRequest memberRequest = new MemberRequest();
+        memberRequest.setEmail("testemail@test.com");
+        memberRequest.setFirstName("test first");
+        memberRequest.setLastName("test Last");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(memberRequest);
+
+        this.mockMVC.perform(post("/members").contentType(MediaType.APPLICATION_JSON)
+                                             .content(json))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().string(containsString("Date of Birth is Required")));
     }
 }
